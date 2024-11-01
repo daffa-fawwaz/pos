ae3f285b0f2c692a07ae1dfef9cab8f2f0681456
daffa-fawwaz <daffafawwaz532@gmail.com>
2024-11-01 19:42:29 +0700

    update

--- search/Category.php
+++ search/Category.php
@@ -21,36 +21,42 @@ if (isset($keyword)) {
 ?>
 
 <div id="container" class="table-responsive">
-    <table class="table table-striped">
-        <tr>
-            <th>
-                <div class="custom-checkbox custom-control">
-                    <input type="checkbox" data-checkboxes="mygroup" data-checkbox-role="dad" class="custom-control-input" id="checkbox-all">
-                    <label for="checkbox-all" class="custom-control-label">&nbsp;</label>
-                </div>
-            </th>
-            <th>Nama Kategori</th>
-            <th>Action</th>
-        </tr>
-        <?php $no = 1 ?>
-        <?php foreach ($kategoris as $categori): ?>
-
+    <?php if ($kategoris == null): ?>
+        <div class="alert alert-danger" role="alert">
+            Data yang anda cari tidak ada!!
+        </div>
+    <?php else: ?>
+        <table class="table table-striped">
             <tr>
-                <td>
+                <th>
                     <div class="custom-checkbox custom-control">
-                        <input type="checkbox" data-checkboxes="mygroup" class="custom-control-input" id="checkbox-<?= $no ?>">
-                        <label for=" checkbox-<?= $no++ ?>" class="custom-control-label">&nbsp;</label>
+                        <input type="checkbox" data-checkboxes="mygroup" data-checkbox-role="dad" class="custom-control-input" id="checkbox-all">
+                        <label for="checkbox-all" class="custom-control-label">&nbsp;</label>
                     </div>
-                </td>
-                <td><?= $categori['name'] ?></td>
-                <td>
-                    <a href="<?= $categori['id'] ?>" class="btn btn-primary">Detail</a>
-                    <a href="<?= $categori['id'] ?>" class="btn btn-success">Edit</a>
-                    <a href="<?= $categori['id'] ?>" class="btn btn-danger">Hapus</a>
-                </td>
+                </th>
+                <th>Nama Kategori</th>
+                <th>Action</th>
             </tr>
-        <?php endforeach ?>
-    </table>
+            <?php $no = 1 ?>
+            <?php foreach ($kategoris as $categori): ?>
+
+                <tr>
+                    <td>
+                        <div class="custom-checkbox custom-control">
+                            <input type="checkbox" data-checkboxes="mygroup" class="custom-control-input" id="checkbox-<?= $no ?>">
+                            <label for=" checkbox-<?= $no++ ?>" class="custom-control-label">&nbsp;</label>
+                        </div>
+                    </td>
+                    <td><?= $categori['name'] ?></td>
+                    <td>
+                        <a href="<?= $categori['id'] ?>" class="btn btn-primary">Detail</a>
+                        <a href="<?= $categori['id'] ?>" class="btn btn-success">Edit</a>
+                        <a href="<?= $categori['id'] ?>" class="btn btn-danger">Hapus</a>
+                    </td>
+                </tr>
+            <?php endforeach ?>
+        </table>
+    <?php endif; ?>
 
 
 
